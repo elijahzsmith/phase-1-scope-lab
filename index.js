034283e37022679088126dcc3ca69cff4dc5a2a6
Elijah Smith <elijahsmith.m@gmail.com>
2022-03-15 14:29:24 -0500

    commit tree 75393bf312a4054abe59deb4816df5ca4105bd69

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+//const upperCaseCustomerName = customerName => customerName.toUpperCase(customerName);
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "john"
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "jim"
+}
